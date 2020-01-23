cbd67a4fcf92b978b668287cc1d22d0951c60c7a
Treagan Birbal <treaganbirbal@pursuit.org>
2020-01-22 23:58:21 -0500

    move 2nd slide left of 1st

--- carousel.js
+++ carousel.js
@@ -7,6 +7,10 @@ const dots = Array.from(dotsNav.children);
 
 const slideWidth = slides[0].getBoundingClientRect().width
 
+//arrange slides next to each other
+slides[0].style.left = 0;
+slides[1].style.left = slideWidth + 'px';
+
 //when I click left move slides to left
 //when I click right move slides to right
 //when I click indicators move that to slide
