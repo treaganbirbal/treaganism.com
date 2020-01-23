a2a6994abb852d3b78cda86e66e00f4be5cd4baf
Treagan Birbal <treaganbirbal@pursuit.org>
2020-01-22 23:50:57 -0500

    get size of photos

--- carousel.js
+++ carousel.js
@@ -1,9 +1,11 @@
 const track = document.querySelector('.carousel_track');
-const slide = Array.from(track.children);
+const slides = Array.from(track.children);
 const  nextButton = document.querySelector('.btn-right');
 const prevButton = document.querySelector('.btn-left');
 const dotsNav = document.querySelector('.carousel-nav');
 const dots = Array.from(dotsNav.children);
+const slideSize = slides[0].getBoundingClientRect()
+console.log(slideSize)
 //when I click left move slides to left
 //when I click right move slides to right
 //when I click indicators move that to slide
