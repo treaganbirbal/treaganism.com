24e4d47582663dd1f409e7d8d8284397dcedba5c
Treagan Birbal <treaganbirbal@pursuit.org>
2020-01-23 00:03:30 -0500

    refactor slides position with forEach loop

--- carousel.js
+++ carousel.js
@@ -8,9 +8,13 @@ const dots = Array.from(dotsNav.children);
 const slideWidth = slides[0].getBoundingClientRect().width
 
 //arrange slides next to each other
-slides[0].style.left = slideWidth * 0 + 'px';
-slides[1].style.left = slideWidth * 1+ 'px';
-slides[2].style.left = slideWidth * 2 + 'px';
+// slides[0].style.left = slideWidth * 0 + 'px';
+// slides[1].style.left = slideWidth * 1+ 'px';
+// slides[2].style.left = slideWidth * 2 + 'px';
+//refactor above to loop thru slides and position
+slides.forEach((slide, idx) =>  {
+    slide.style.left = slideWidth * idx + 'px'
+})
 
 //when I click left move slides to left
 //when I click right move slides to right
