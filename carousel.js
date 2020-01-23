fd67037f911015bb9d657f72c14ae44cc2b4fddc
Treagan Birbal <treaganbirbal@pursuit.org>
2020-01-22 23:47:37 -0500

    select dotsNav children and save to variable-dots

--- carousel.js
+++ carousel.js
@@ -3,6 +3,7 @@ const slide = Array.from(track.children);
 const  nextButton = document.querySelector('.btn-right');
 const prevButton = document.querySelector('.btn-left');
 const dotsNav = document.querySelector('.carousel-nav');
+const dots = Array.from(dotsNav.children);
 //when I click left move slides to left
 //when I click right move slides to right
 //when I click indicators move that to slide
