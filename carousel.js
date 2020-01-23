e44eff53a157342e58346b8c416700af65bbac71
Treagan Birbal <treaganbirbal@pursuit.org>
2020-01-23 00:01:01 -0500

    refactor img location

--- carousel.js
+++ carousel.js
@@ -8,8 +8,9 @@ const dots = Array.from(dotsNav.children);
 const slideWidth = slides[0].getBoundingClientRect().width
 
 //arrange slides next to each other
-slides[0].style.left = 0;
-slides[1].style.left = slideWidth + 'px';
+slides[0].style.left = slideWidth * 0 + 'px';
+slides[1].style.left = slideWidth * 1+ 'px';
+slides[2].style.left = slideWidth * 2 + 'px';
 
 //when I click left move slides to left
 //when I click right move slides to right
