ff0fb0d22e253b774a2f780be0fd762e46bf641e
Treagan Birbal <treaganbirbal@pursuit.org>
2020-01-22 23:46:15 -0500

    select corrent element for dotsNav

--- carousel.js
+++ carousel.js
@@ -2,7 +2,7 @@ const track = document.querySelector('.carousel_track');
 const slide = Array.from(track.children);
 const  nextButton = document.querySelector('.btn-right');
 const prevButton = document.querySelector('.btn-left');
-const dotsNav = document.querySelector('.carousel_nav_btn')
+const dotsNav = document.querySelector('.carousel-nav');
 //when I click left move slides to left
 //when I click right move slides to right
 //when I click indicators move that to slide
