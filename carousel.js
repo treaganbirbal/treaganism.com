242829a59748d999cf37e307047dcc84c1fe41e9
Treagan Birbal <treaganbirbal@pursuit.org>
2020-01-22 23:55:10 -0500

    rm console.log

--- carousel.js
+++ carousel.js
@@ -4,9 +4,9 @@ const  nextButton = document.querySelector('.btn-right');
 const prevButton = document.querySelector('.btn-left');
 const dotsNav = document.querySelector('.carousel-nav');
 const dots = Array.from(dotsNav.children);
+
 const slideWidth = slides[0].getBoundingClientRect().width
 
-console.log(slideWidth)
 //when I click left move slides to left
 //when I click right move slides to right
 //when I click indicators move that to slide
